e117d47574bc597330cc9b1bbea8937fa3894aa7
Marco Franssen <marco.franssen@gmail.com>
2020-01-03 09:47:52 +0100

    Implement targets endpoint by piping stdout to response

--- targets/targets.go
+++ targets/targets.go
@@ -1,7 +1,9 @@
 package targets
 
 import (
+	"io"
 	"net/http"
+	"os/exec"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
@@ -18,8 +20,36 @@ func RegisterRoutes(r chi.Router) {
 }
 
 func listTargets(w http.ResponseWriter, r *http.Request) {
-	if err := render.Render(w, r, e.ErrNotImplemented); err != nil {
+	cmd := exec.Command("notary", "key", "export")
+	pipeReader, pipeWriter := io.Pipe()
+	defer pipeWriter.Close()
+
+	cmd.Stdout = pipeWriter
+	cmd.Stderr = pipeWriter
+	go pipeResponse(w, pipeReader)
+
+	err := cmd.Run()
+	if err != nil {
 		render.Render(w, r, e.ErrRender(err))
+	}
+}
+
+func pipeResponse(w http.ResponseWriter, reader io.ReadCloser) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := reader.Read(buf)
+		if err != nil {
+			reader.Close()
+			break
+		}
+		data := buf[0:n]
+		w.Write(data)
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+		}
+		for i := 0; i < n; i++ {
+			buf[i] = 0
+		}
 	}
 }
 
