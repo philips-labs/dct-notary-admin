f9818eff3bfcfbb5762b15e7c87159bfa015ced2
Marco Franssen <marco.franssen@gmail.com>
2020-01-22 10:22:56 +0100

    Add Trustpinning configuration

--- lib/notary/service.go
+++ lib/notary/service.go
@@ -39,6 +39,7 @@ type Service struct {
 type Config struct {
 	TrustDir     string             `json:"trust_dir" mapstructure:"trust_dir"`
 	RemoteServer RemoteServerConfig `json:"remote_server" mapstructure:"remote_server"`
+	TrustPinning TrustPinningConfig `json:"trust_pinning" mapstructure:"trust_pinning"`
 }
 
 // RemoteServerConfig notary remote server configuration
@@ -48,6 +49,13 @@ type RemoteServerConfig struct {
 	TLSClientKey  string `json:"tls_client_key" mapstructure:"tls_client_key"`
 	TLSClientCert string `json:"tls_client_cert" mapstructure:"tls_client_cert"`
 	SkipTLSVerify bool   `json:"skipTLSVerify" mapstructure:"skipTLSVerify"`
+}
+
+// TrustPinningConfig notary trust pinning configuration
+type TrustPinningConfig struct {
+	DisableTofu bool                   `json:"disable_tofu" mapstructure:"disable_tofu"`
+	CA          map[string]string      `json:"ca" mapstructure:"ca"`
+	Certs       map[string]interface{} `json:"certs" mapstructure:"certs"`
 }
 
 // NewService creates a new notary service object
