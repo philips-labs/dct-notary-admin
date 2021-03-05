7bf6bb6ed3d34022ede605eef02ef5b4bf027567
Marco Franssen <marco.franssen@gmail.com>
2021-03-05 19:35:34 +0100

    Replace grommet list with tailwind

--- web/src/pages/Targets/index.tsx
+++ web/src/pages/Targets/index.tsx
@@ -4,7 +4,7 @@ import { Delegations, Targets } from '../../components';
 
 export const TargetsPage: FC = () => {
   return (
-    <div className="overflow-auto grid lg:grid-cols-2 gap-10">
+    <div className="overflow-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
       <div>
         <h2 className="text-4xl tracking-tight font-bold">Targets</h2>
         <Targets />
